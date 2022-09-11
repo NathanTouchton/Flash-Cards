19638219efe633b8acc9c89ec2169177ef5d42f9
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-09-11 18:03:03 -0500

    Delete flash-cards directory

--- flash-cards/flash-cards/main.py
+++
@@ -1,6 +0,0 @@
-BACKGROUND_COLOR = "#B1DDC6"
-
-# flip card after 3 seconds
-# check and x are for wether or not you know the answer
-# green for english, white for french
-# if you know the answer, the entry gets deleted from the list
