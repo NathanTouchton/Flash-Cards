48aa9b967d31df3ff2cd3f95d86c7df969ea4279
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-09-14 18:43:31 -0500

    Add files via upload

--- flash-cards/main.py
+++ flash-cards/main.py
@@ -1,6 +1,9 @@
+from tkinter import Button, Label, Canvas, PhotoImage
 BACKGROUND_COLOR = "#B1DDC6"
 
 # flip card after 3 seconds
 # check and x are for wether or not you know the answer
 # green for english, white for french
 # if you know the answer, the entry gets deleted from the list
+
+
