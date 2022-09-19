713ec7971b788b1d8718012bfb23d5e12cf4bf71
Nathan Touchton <ntouchton@protonmail.com>
2022-09-18 21:02:48 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -9,19 +9,23 @@ BACKGROUND_COLOR = "#B1DDC6"
 # if you know the answer, the entry gets deleted from the list
 
 # AI for switching between languages
-def change_language():
+def change_language_english():
     flash_card.itemconfig(language, text="English")
     flash_card.itemconfig(background, image=back)
 
-# AI for the word picker
+def change_language_french():
+    flash_card.itemconfig(language, text="French")
+    flash_card.itemconfig(background, image=front)
 
+# AI for the word picker
 word_list = read_csv("french_words.csv")
 
 def change_word_check():
     global word_list
-    current_word = word_list["French"][randint(0, len(word_list.index - 2))]
+    current_word = word_list["French"][randint(0, len(word_list) - 1)]
     flash_card.itemconfig(word, text=current_word)
     word_list = word_list.drop(word_list.index[word_list.French == current_word])
+    change_language_french()
 
 # TODO 1: Make the card change back to french every round
 # TODO 2: Write code for change_word_x()
@@ -41,18 +45,18 @@ flash_card = Canvas(width=800, height=526)
 background = flash_card.create_image(400, 263, image=front)
 language = flash_card.create_text(400, 150, text="French", font=("Arial", 40, "italic"))
 word = flash_card.create_text(400, 263,
-    text=word_list["French"][randint(0, len(word_list.index - 2))], font=("Arial", 60, "bold"))
+    text=word_list["French"][randint(0, len(word_list) - 1)], font=("Arial", 60, "bold"))
 
 flash_card.grid(column=0, row=0, columnspan=2)
 
-wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
-wrong_button.grid(column=0, row=1)
+x_image = PhotoImage(file="images/wrong.png")
+x_button = Button(image=x_image, highlightthickness=0)
+x_button.grid(column=0, row=1)
 
-right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=change_word_check)
-right_button.grid(column=1, row=1)
+check_image = PhotoImage(file="images/right.png")
+check_button = Button(image=check_image, highlightthickness=0, command=change_word_check)
+check_button.grid(column=1, row=1)
 
-flash_card.after(3000, change_language)
+flash_card.after(3000, change_language_english)
 
 window.mainloop()
